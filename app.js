6c23c67efbc07fd769c71d961ee4dc1d91f81abb
rivu <58026024+unphydra@users.noreply.github.com>
2020-01-25 19:38:35 +0530

    guest book now contain comments and also show comment

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const Response = require('./lib/response');
 const CONTENT_TYPES = require('./lib/mimeTypes');
+const commentList = require('./commentList.json');
 
 const STATIC_FOLDER = `${__dirname}/public`;
 
@@ -31,7 +32,49 @@ const serveHomePage = req => {
   return res;
 };
 
+const serveGuestBook = function(req) {
+  if (req.method == 'POST') {
+    let date = new Date().toJSON().slice(0, -5);
+    date = date.replace(/T/, ' ');
+    let { name, commentMsg } = req.body;
+    commentMsg = decodeURIComponent(commentMsg);
+    commentMsg = commentMsg.replace(/\+/g, ' ');
+    commentList.push({ name, commentMsg, date });
+    fs.writeFileSync(
+      './commentList.json',
+      JSON.stringify(commentList),
+      'utf8'
+    );
+  }
+  const html = getGustBookHtml(commentList);
+  const res = new Response();
+  res.setHeader('Content-Type', CONTENT_TYPES.html);
+  res.setHeader('Content-Length', html.length);
+  res.statusCode = 200;
+  res.body = html;
+  return res;
+};
+
+const getGustBookHtml = function(commentList) {
+  let divs = `<div class="eachComment">
+  <div class="commentName">Date Name</div>
+  <div>Comment</div>
+</div>`;
+  commentList.forEach(comment => {
+    const dateAndName = `<div class="commentName">${comment.date} -> ${comment.name}</div>`;
+    const msg = `<p>${comment.commentMsg}</p>`;
+    divs += `<div class="eachComment">
+    ${dateAndName}
+    ${msg}
+  </div>`;
+  });
+  let content = fs.readFileSync('./public/html/guestBook.html', 'utf8');
+  const pattern = new RegExp(`__comment__`, 'g');
+  return content.replace(pattern, divs);
+};
+
 const findHandler = req => {
+  if (req.url === '/html/guestBook.html') return serveGuestBook;
   if (req.method === 'GET') {
     if (req.url === '/') return serveHomePage;
     return serveStaticFile;
