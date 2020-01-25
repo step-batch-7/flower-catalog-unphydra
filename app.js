c73395f3db48c883a4550b19113b082bf8f219a2
rivu <58026024+unphydra@users.noreply.github.com>
2020-01-25 15:45:10 +0530

    home page and all other sub pages are connected

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const fs = require('fs');
 const Response = require('./lib/response');
 const CONTENT_TYPES = require('./lib/mimeTypes');
-const { loadTemplate } = require('./lib/viewTemplate');
 
 const STATIC_FOLDER = `${__dirname}/public`;
-let visitorCount = 0;
 
 const serveStaticFile = req => {
   const path = `${STATIC_FOLDER}${req.url}`;
@@ -32,28 +30,12 @@ const serveHomePage = req => {
   res.body = html;
   return res;
 };
-const generateStudentResponse = student => {
-  const html = loadTemplate('student.html', student);
-  const res = new Response();
-  res.setHeader('Content-Type', CONTENT_TYPES.html);
-  res.setHeader('Content-Length', html.length);
-  res.statusCode = 200;
-  res.body = html;
-  return res;
-};
-const registerStudent = req => {
-  return generateStudentResponse(req.query);
-};
-const registerStudentPost = req => {
-  return generateStudentResponse(req.body);
-};
+
 const findHandler = req => {
-  if (req.method === 'GET' && req.url === '/') return serveHomePage;
-  // if (req.method === 'GET' && req.url === '/visitorCount')
-  //   return serveVisitorCount;
-  // if (req.method === 'GET' && req.url === '/registerStudent')
-  //   return registerStudent;
-  if (req.method === 'GET') return serveStaticFile;
+  if (req.method === 'GET') {
+    if (req.url === '/') return serveHomePage;
+    return serveStaticFile;
+  }
   return () => new Response();
 };
 const processRequest = req => {
